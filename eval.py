c7888fb312a80a3b76aa39cfcd2e4d38ae7da502
Plankton Project <plankton@gpu1.svcl.ucsd.edu>
2017-08-17 10:52:16 -0700

    Added identification tags with each file name creation

--- eval.py
+++ eval.py
@@ -3,6 +3,7 @@ import cPickle as pickle
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import numpy as np
 from sklearn.metrics import *
 import caffe
@@ -12,6 +13,7 @@ import os
 import timeit
 import csv
 import sys
+import cPickle as pickle
 
 # Specify which source domain & classifier will be used for evaluating the target domain
 source = 'spcinsitu'
@@ -19,8 +21,7 @@ classifier = 'insitu_finetune'
 exp_num = 'exp2'
 model = 'model_' + exp_num + '.caffemodel'
 domain_path = os.path.join ('/data4/plankton_wi17/mpl/source_domain/', source, classifier)
-temp_outroot = '/data4/plankton_wi17/mpl/source_domain/spcinsitu/insitu_finetune/code'
-outroot = os.path.join('/data4/plankton_wi17/mpl/source_domain',source,classifier,exp_num)
+outroot = os.path.join('/data4/plankton_wi17/mpl/target_domain',source,classifier,exp_num)
 # domain_path = lab_google_root
 
 
@@ -125,8 +126,8 @@ def plot_roc_curve(gt,prob,classifier,num_class):
 
     #fpr["micro"], tpr["micro"], _ = roc_curve (gtruth.ravel (), pred.ravel ())
 
-    #fpr["micro"], tpr["micro"], _ = roc_curve (gtruth.ravel (), prob.ravel ())
-    #roc_auc["micro"] = auc (fpr["micro"], tpr["micro"])
+    fpr["micro"], tpr["micro"], _ = roc_curve (gtruth.ravel (), prob.ravel ())
+    roc_auc["micro"] = auc (fpr["micro"], tpr["micro"])
 
     plt.figure()
     lw = 2
@@ -140,7 +141,7 @@ def plot_roc_curve(gt,prob,classifier,num_class):
     plt.ylabel('True Positive Rate')
     plt.title('Receiver Operating Characteristic')
     plt.legend(loc="lower right")
-    plt.savefig(outroot + '/' + classifier + '-' + exp_num +'_curve.png')
+    plt.savefig(outroot + '/' + classifier+'_curve.png')
     print (classifier+'_curve.png saved.')
 
 def compute_cmatrix(gtruth,pred,num_class):
@@ -172,7 +173,7 @@ def main(test_data, num_class, domain, classifier, model):
     t1 = timeit.default_timer() # Start timer
 
     # Load LMDB
-    images, labels = load_lmdb(temp_outroot + '/' + test_data)
+    images, labels = load_lmdb(test_data)
 
     # Set to GPU mode
     caffe.set_mode_gpu()
@@ -187,6 +188,8 @@ def main(test_data, num_class, domain, classifier, model):
         raise ValueError (os.path.basename(deploy_proto) + " not found")
     elif not os.path.exists(trained_weights):
         raise ValueError (os.path.basename(trained_weights) + " not found")
+    elif not os.path.exists(outroot):
+        os.makedirs(outroot)
 
     # Load net
     deploy = caffe.Net(deploy_proto,caffe.TEST, weights=trained_weights)
@@ -236,8 +239,21 @@ def main(test_data, num_class, domain, classifier, model):
     print (predictions[0:25])
     print ('Total Accuracy', total_accu)
 
+    # # write prediction results to a txt file
+    # pred_file_name = os.path.join(outroot,classifier+'_target_preds.txt')
+    # results_txt = open(pred_file_name, 'w')
+    # for i in range(len(gtruth)):
+    #     results_txt.write(str(probs[i,0])+' '+str(probs[i,1]))
+    #     results_txt.write(' ')
+    #     results_txt.write(str(predictions[i]))
+    #     results_txt.write('\n')
+    # results_txt.close()
+
     # Write predictions to img path lbl csv file
-    write_pred2csv(predictions,probs)
+    write_pred2csv(predictions,probs,classifier)
+
+    # Plot ROC Curve
+    plot_roc_curve(gtruth,probs,classifier,num_class)
 
     # Create array for confusion matrix with dimensions based on number of classes
     confusion_matrix_count = np.zeros ((num_class, num_class))
@@ -253,22 +269,35 @@ def main(test_data, num_class, domain, classifier, model):
         confusion_matrix_rate[i,:] = (confusion_matrix_count[i,:])/class_count[i,0]
     confusion_matrix_rate = np.around(confusion_matrix_rate, decimals=4)
 
-    results_filename = os.path.join (outroot, classifier + '-' + exp_num + '_Results.csv')
+    # Calculate Precision Rate
+    precision = (confusion_matrix_count[0,0]/(confusion_matrix_count[0,0]+confusion_matrix_count[1,0]))*100 # TP / (FP+TP)
+    print("Precision {}".format(precision))
+
+    # Calculate side lobes
+    S = np.sort (probs)
+    S = S[::-1]
+    confidence_level = [(S[i, 1] - S[i, 0]) / S[i, 1] for i in range (len (S))]
+    confidence_level = np.asarray(confidence_level)
+    avg_confidence = confidence_level.mean()*100
+    print ("Confidence Level {}".format(avg_confidence))
+
+    results_filename = os.path.join (outroot, classifier + '_Results.csv')
     outfile = open (results_filename, 'wb')
     writer = csv.writer (outfile, delimiter=",")
     writer.writerow (['Binary Classifier: ' + domain.split ('/')[6]])
     writer.writerow (['Total Accuracy'])
     writer.writerow ([str (total_accu)])
+    writer.writerow (['Precision Rate'])
+    writer.writerow ([str (precision)])
+    writer.writerow (['Confidence Level'])
+    writer.writerow ([str (avg_confidence)])
     writer.writerow (['Prediction Results:'])
-    np.savetxt (outfile, confusion_matrix_count, delimiter=",")
+    np.savetxt (outfile, confusion_matrix_count,'%5.2f', delimiter=",")
     writer.writerow (['Confusion Matrix:'])
-    np.savetxt (outfile, confusion_matrix_rate * 100, delimiter=",")
+    np.savetxt (outfile, confusion_matrix_rate * 100,'%5.2f', delimiter=",")
     print ('Print to', results_filename, 'file successful.')
 
-    # Plot ROC Curve
-    plot_roc_curve(gtruth,probs,classifier,num_class)
-
-def write_pred2csv(predictions, probs):
+def write_pred2csv(predictions, probs, classifier):
     '''
     Write predictions and confidence level to csv file to upload to server
     :param predictions: predictions outputted from classifier
@@ -277,21 +306,87 @@ def write_pred2csv(predictions, probs):
     '''
 
     # Load dataframe
-    file_name = temp_outroot + '/test.txt'
-    df = pd.read_csv(file_name,sep=' ' ,header=None)
-    df.columns = ['path','gtruth']
+    file_name = 'master-fourhr_target_image_path_labels.csv'
+    df1 = pd.read_csv(file_name,index_col=0)
 
     # Add predictions to additional column
-    df['predictions']= predictions
+    df1['predictions']= predictions
 
     # Calculate side lobes
     S = np.sort(probs)
     S = S[::-1]
     confidence_level = [(S[i,1]-S[i,0])/S[i,1] for i in range(len(S))]
-    df['confidence_level'] = confidence_level
+    df1['confidence_level'] = confidence_level
 
     # Save changes to csv output
-    df.to_csv(outroot + '/' + classifier + '-' + exp_num + '_target_image_path_labels.csv')
+    df1.to_csv(outroot + '/' + classifier + '_preds.csv')
 
+def temp():
+    # Access text files for gtruth and pred
+    with open (outroot + '/' + classifier +'_target_preds.txt', 'r') as f:
+        df = pd.read_csv (f, sep=" ", header=None)
+        df.columns = ['prob1', 'prob2', 'pred']
+    f.close ()
+    with open ('target_image_paths_labels.txt', 'r') as f_target:
+        df1 = pd.read_csv (f_target, sep=" ", header=None)
+        df1.columns = ['path', 'gtruth']
+    f_target.close ()
+
+    gtruth_list = df1['gtruth'].tolist()
+    pred_list = df['pred'].tolist()
+    path_list = df1['path'].tolist()
+
+    fig = plt.figure(1,figsize=(15, 15))
+    # True positive images [0,0]
+    view_classified_img(fig, gtruth_list,pred_list,path_list, 0, 0, 1)
+    #view_classified_img(gtruth_list,pred_list,path_list, 0, 0, 1,classifier+'_true_positive.png')
+
+    # True negative images [1,1]
+    view_classified_img (fig, gtruth_list, pred_list, path_list, 1, 1, 11)
+    #view_classified_img (gtruth_list, pred_list, path_list, 1, 1, 2,lassifier+'_true_negative.png')
+
+    # False positive images [1,0]
+    view_classified_img (fig, gtruth_list, pred_list, path_list, 1, 0, 21)
+    #iew_classified_img (gtruth_list, pred_list, path_list, 1, 0, 3, classifier + '_false_positive.png')
+
+    # False negative images [0,1]
+    view_classified_img (fig, gtruth_list, pred_list, path_list, 0, 1, 31)
+    #view_classified_img (gtruth_list, pred_list, path_list, 0, 1, 4, classifier + '_false_negative.png')
+
+    plt.savefig(outroot + '/' + classifier+'_img_summary', bbox_inches='tight')
+
+def view_classified_img(fig,gtruth,pred,path,tf,pn,position):
+    '''
+
+     :param fig: figure to save all results on
+     :param gtruth: ground truth labels
+     :param pred: prediction results
+     :param path: server path to image
+     :param tf: true --> 0, false --> 1
+     :param pn: positive --> 0. negative --> 1
+     :param position: position to begin plotting images in row/col of figure
+     :return: 
+     '''
+    n_row = 4  # number of outcomes on confusion matrix
+    n_col = 10  # number of images per row
+    end_row = position + 10
+    for i in range (len (pred)):
+
+        # Based off of true/false postive/negative, add image to subplot
+        if gtruth[i] == tf and pred[i] == pn:
+            img = mpimg.imread (path[i])
+            fig.add_subplot (n_row, n_col, position)
+            plt.imshow (img)
+            plt.axis ('off')
+
+            # Title: Index;Gtruth;Pred
+            title = str (i + 1) + ';' + str (gtruth[i]) + ';' + str (pred[i])
+            plt.title (title)
+            position += 1
+
+            # Stop collecting images after end of the row (10 columns)
+            if position == end_row:
+                break
 if __name__=='__main__':
-    main ('test1.LMDB', 2, domain_path, classifier, model)
+    main ('target_fourhrs.LMDB', 2, domain_path, classifier, model)
+    #temp()
